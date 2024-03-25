625531ffe18e793c24e96e8527724d5d738fff7b
Megagig <megagigsoftwaresolutions.com>
2024-03-25 23:59:26 +0100

    Setup the Homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,37 @@
+import backgroundImage from '../../public/home/homebg.jpg';
 import Image from 'next/image';
+import { SparklesIcon } from '@heroicons/react/24/solid';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center space-y-8">Welcome</div>
-    </main>
+    <div className="flex relative flex-col justify-center items-center h-screen">
+      <Image
+        className="blur-sm"
+        alt="Background"
+        src={backgroundImage}
+        placeholder="blur"
+        quality={100}
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: 'cover',
+        }}
+      />
+      <div className=" p-2 flex flex-col justify-center items-center z-10 bg-purple-100 lg:w-2/5 h-1/4 rounded-md">
+        <div className="flex pb-5 mb-5 text-5xl text-purple-800">
+          <SparklesIcon className="w-12 h-12" />
+          <p>Savio 96 Alumni Association</p>
+        </div>
+        <div>
+          <Link
+            href="/store"
+            className="bg-purple-700 text-white hover:bg-purple-900 p-3 rounded-sm text-lg text mr-2"
+          >
+            Click To See The Members
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
