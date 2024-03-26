e13b8b076d07eebacfc3f1a538fee6b38493a5a2
Megagig <megagigsoftwaresolutions.com>
2024-03-26 09:45:40 +0100

    create a fake data with JSON

--- src/app/members/page.jsx
+++ src/app/members/page.jsx
@@ -10,7 +10,6 @@ const MembersListPage = () => {
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {members.map((member) => (
-              // <div>{member.name}</div>
               <MemberCard key={member._id} member={member} />
             ))}
           </div>
